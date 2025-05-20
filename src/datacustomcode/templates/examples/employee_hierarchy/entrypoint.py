af66574262e3e3f95ac7fcde97a3f91db6bfd01a
Christy Cao <yijun.cao@salesforce.com>
2025-05-20 08:43:25 -0700

    run precommit

--- src/datacustomcode/templates/examples/employee_hierarchy/entrypoint.py
+++ src/datacustomcode/templates/examples/employee_hierarchy/entrypoint.py
@@ -1,4 +1,8 @@
-from pyspark.sql.functions import col, lit, concat_ws
+from pyspark.sql.functions import (
+    col,
+    concat_ws,
+    lit,
+)
 
 from datacustomcode.client import Client
 from datacustomcode.io.writer.base import WriteMode
@@ -8,45 +12,55 @@ def main():
     client = Client()
 
     employees = client.read_dlo("Employee__dll").persist()
-    employees = employees.select(
-        "id__c",
-        "manager_id__c",
-        "name__c",
-        "position__c"
-    )
+    employees = employees.select("id__c", "manager_id__c", "name__c", "position__c")
     employees.show()
-    employees_with_manager = employees.alias("e") \
-        .join(employees.alias("m"), col("e.manager_id__c").cast("string") == col("m.id__c").cast("string"), "left") \
+    employees_with_manager = (
+        employees.alias("e")
+        .join(
+            employees.alias("m"),
+            col("e.manager_id__c").cast("string") == col("m.id__c").cast("string"),
+            "left",
+        )
         .select(
             col("e.id__c"),
             col("e.name__c"),
             col("e.position__c"),
             col("e.manager_id__c"),
-            col("m.name__c").alias("manager_name__c")
-        ).persist()
+            col("m.name__c").alias("manager_name__c"),
+        )
+        .persist()
+    )
 
-    hierarchy_df = employees_with_manager.filter(col("manager_id__c").isNull()) \
-        .withColumn("hierarchy_level__c", lit(1)) \
-        .withColumn("management_chain__c", col("name__c")).persist()
+    hierarchy_df = (
+        employees_with_manager.filter(col("manager_id__c").isNull())
+        .withColumn("hierarchy_level__c", lit(1))
+        .withColumn("management_chain__c", col("name__c"))
+        .persist()
+    )
 
     current_level = 1
 
     while True:
         ewm = employees_with_manager.alias("ewm")
-        hdf = hierarchy_df.filter(col("hierarchy_level__c") == current_level).alias("hdf")
+        hdf = hierarchy_df.filter(col("hierarchy_level__c") == current_level).alias(
+            "hdf"
+        )
 
-        next_level_df = (ewm
-                         .join(hdf, col("ewm.manager_id__c").cast("string")  == col("hdf.id__c").cast("string") , "inner")
-                         .select(
-                             col("ewm.id__c"),
-                             col("ewm.name__c"),
-                             col("ewm.position__c"),
-                             col("ewm.manager_id__c"),
-                             col("ewm.manager_name__c"),
-                             (col("hdf.hierarchy_level__c") + 1).alias("hierarchy_level__c"),
-                             concat_ws(" | ", col("hdf.management_chain__c"), col("ewm.name__c")).alias("management_chain__c")
-                         )
-                        )
+        next_level_df = ewm.join(
+            hdf,
+            col("ewm.manager_id__c").cast("string") == col("hdf.id__c").cast("string"),
+            "inner",
+        ).select(
+            col("ewm.id__c"),
+            col("ewm.name__c"),
+            col("ewm.position__c"),
+            col("ewm.manager_id__c"),
+            col("ewm.manager_name__c"),
+            (col("hdf.hierarchy_level__c") + 1).alias("hierarchy_level__c"),
+            concat_ws(" | ", col("hdf.management_chain__c"), col("ewm.name__c")).alias(
+                "management_chain__c"
+            ),
+        )
 
         if next_level_df.isEmpty():
             break
